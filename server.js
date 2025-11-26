921918b6798c4e5cd2399e3740b7e707f2e0e43b
Abhishek Pavithran <abhishekpavi6@gmail.com>
2025-11-26 15:16:34 +0530

    env

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const contactRoutes = require('./routes/contactRoutes');
 const app =express()
 connectDB()
 
-app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
+app.use(cors());
 app.use(express.json())
 app.use('/uploads', express.static('uploads'));
 
